9a4b8861590d276b666ddf4693c88aa1687970b4
BeingAnonymous4u <155914718+BeingAnonymous4u@users.noreply.github.com>
2024-09-12 09:53:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -209,7 +209,7 @@ Nᴀᴍᴇ - {}"""
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
     CAPTION = """<b>📂<u>Fɪʟᴇɴᴀᴍᴇ</u> :\n<a href="https://t.me/+bEarHRzuylxjMTk1">{file_name}</a>  
-<pre>🏷 <u>FɪʟᴇSize</u> : {file_size}</pre></b>""" 
+<pre><blockquote><u>FɪʟᴇSize</u> : {file_size}</pre></blockquote></b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
